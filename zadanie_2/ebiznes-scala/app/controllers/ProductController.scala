e0c6fba681b5dc018d93358aa923e8a4bf0866b5
SwiftFranek <127611343+02franek@users.noreply.github.com>
2026-03-20 19:10:56 +0100

    Zadanie_2/3.5

--- zadanie_2/ebiznes-scala/app/controllers/ProductController.scala
+++ zadanie_2/ebiznes-scala/app/controllers/ProductController.scala
@@ -16,4 +16,46 @@ class ProductController @Inject()(val controllerComponents: ControllerComponents
     def getAll() = Action { implicit request: Request[AnyContent] =>
         Ok(Json.toJson(productList))
     }
+    
+    def getById(id: Long) = Action { implicit request: Request[AnyContent] =>
+        productList.find(_.id == id) match {
+            case Some(product) => Ok(Json.toJson(product))
+            case None => NotFound(Json.obj("error" -> s"Product with id $id not found"))
+        }
+    }
+    
+    def add() = Action { implicit request: Request[AnyContent] =>
+        request.body.asJson.map(_.validate[Product] match {
+            case JsSuccess(product, _) =>
+                productList += product
+                Created(Json.toJson(product))
+            case JsError(_) =>
+                BadRequest(Json.obj("error" -> "Invalid JSON format"))
+        }).getOrElse(BadRequest(Json.obj("error" -> "JSON data is required")))
+    }
+    
+    def update(id: Long) = Action { implicit request: Request[AnyContent] =>
+        request.body.asJson.map(_.validate[Product] match {
+            case JsSuccess(updatedProduct, _) =>
+                val index = productList.indexWhere(_.id == id)
+                if (index != -1) {
+                    productList.update(index, updatedProduct)
+                    Ok(Json.toJson(updatedProduct))
+                } else {
+                    NotFound(Json.obj("error" -> s"Product with id $id not found"))
+                }
+            case JsError(_) => BadRequest(Json.obj("error" -> "Invalid JSON format"))
+        }).getOrElse(BadRequest(Json.obj("error" -> "JSON data is required")))
+    }
+
+    def delete(id: Long) = Action { implicit request: Request[AnyContent] =>
+        val index = productList.indexWhere(_.id == id)
+        if (index != -1) {
+            productList.remove(index)
+            Ok(Json.obj("message" -> s"Product with id $id deleted successfully"))
+        } else {
+            NotFound(Json.obj("error" -> s"Product with id $id not found"))
+        }
+    }
+
 }
