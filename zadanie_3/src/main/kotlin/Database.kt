8af3b2d30f6a1ebaa63f200884c3dc449b64578e
SwiftFranek <127611343+02franek@users.noreply.github.com>
2026-05-31 16:44:32 +0200

    Zadanie_3/4.5

--- zadanie_3/src/main/kotlin/Database.kt
+++ zadanie_3/src/main/kotlin/Database.kt
@@ -1,10 +1,19 @@
 object Database {
     val categories = listOf(
         "Electronics",
-        "Household items",
-        "Baby items",
+        "Household",
+        "Baby",
         "Sports",
         "Games",
         "Food"
+    )
+
+    val products = mapOf(
+        "electronics" to listOf("Laptop", "Gaming mouse", "Keyboard", "Headphones", "iPad"),
+        "household" to listOf("Cups", "Garbage bin", "Paper towels"),
+        "baby" to listOf("Toys", "Stroller", "Milk formula"),
+        "sports" to listOf("Ball", "Sneakers", "Fins"),
+        "games" to listOf("Donkey Kong Country", "Gex", "Gothic 2"),
+        "food" to listOf("Burgir", "Pizza", "Flour", "Salt", "Pepper")
     )
 }
--- zadanie_3/src/main/kotlin/MessageHandler.kt
+++ zadanie_3/src/main/kotlin/MessageHandler.kt
@@ -1,11 +1,28 @@
 class MessageHandler {
     fun handle(command: String): String? {
-        return when (command) {
-            "!hello" -> "world!"
+        val parts = command.trim().split("\\s+".toRegex())
+        val baseCommand = parts[0].lowercase()
+
+        return when (baseCommand) {
             "!categories" -> {
                 val list = Database.categories.joinToString(separator = "\n") { "  $it" }
                 "List of available categories:\n$list"
             }
+
+            "!products" -> {
+                if (parts.size < 2) { return "Usage: `!products <Category>`" }
+                
+                val requestedCategory = parts[1].lowercase()
+                val products = Database.products[requestedCategory]
+
+                if (products != null) {
+                    val list = products.joinToString(separator="\n") { "  $it" }
+                    "$requestedCategory products:\n$list"
+                } else {
+                    "Category $requestedCategory not found. To see available categories type: `!categories`"
+                }
+            }
+
             else -> null
         }
     }
