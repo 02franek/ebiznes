ced3a738ac650a29af102b20d4660c73be754ff1
SwiftFranek <127611343+02franek@users.noreply.github.com>
2026-05-31 19:26:12 +0200

    Zadanie_4/3.5

--- zadanie_4/zadanie-echo/main.go
+++ zadanie_4/zadanie-echo/main.go
@@ -5,68 +5,100 @@ import (
 	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 type Product struct {
-	ID int `json:"id"`
+	gorm.Model
 	Name string `json:"name"`
 	Price float32 `json:"price"`
 }
-
-var (
-	products = map[int]*Product{}
-	nextID = 1
-)
 
 func createProduct(c echo.Context) error {
 	p := new(Product)
 	if err := c.Bind(p); err != nil {
 		return err
 	}
-	p.ID = nextID
-	products[nextID] = p
-	nextID++
+	
+	if result := db.Create(&p); result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "Error creating product",
+		})
+	}
+
 	return c.JSON(http.StatusCreated, p)
 }
 
 func getProducts(c echo.Context) error {
-	list := make([]*Product, 0, len(products))
-	for _, p := range products {
-		list = append(list, p)
+	var products []Product
+
+	if result := db.Find(&products); result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "Error loading products",
+		})
 	}
-	return c.JSON(http.StatusOK, list)
+	
+	return c.JSON(http.StatusOK, products)
 }
 
 func getProduct(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	if p, ok := products[id]; ok {
-		return c.JSON(http.StatusOK, p)
+	var p Product
+
+	if result := db.First(&p, id); result.Error != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{
+			"message": "Product not found",
+		})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"message": "Product not found"})
+	
+	return c.JSON(http.StatusOK, p)
 }
 
 func updateProduct(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	if p, ok := products[id]; ok {
-		if err := c.Bind(p); err != nil {
-			return err
-		}
-		p.ID = id
-		return c.JSON(http.StatusOK, p)
+	var p Product
+
+	if result := db.First(&p, id); result.Error != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{
+			"message": "Product not found",
+		})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"message": "Product not found"})
+
+	if err := c.Bind(&p); err != nil {
+		return err
+	}
+
+	db.Save(&p)
+	return c.JSON(http.StatusOK, p)
 }
 
 func deleteProduct(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
-	if _, ok := products[id]; ok {
-		delete(products, id)
-		return c.NoContent(http.StatusNoContent)
+	var p Product
+
+	if result := db.First(&p, id); result.Error != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{
+			"message": "Product not found",
+		})
 	}
-	return c.JSON(http.StatusNotFound, map[string]string{"message": "Product not found"})
+
+	db.Delete(&p, id)
+	return c.NoContent(http.StatusNoContent)
 }
 
 func main() {
+	var err error
+
+	db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		panic("Couldn't establish connection with data base")
+	}
+
+	db.AutoMigrate(&Product{})
+
 	e := echo.New()
 
 	e.POST("/products", createProduct)
